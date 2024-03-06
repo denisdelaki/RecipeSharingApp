1892668f51d6ae003847d93d279dd505b21c0612
denisdelaki <denisdelaki@gmail.com>
2024-03-06 16:45:21 +0300

    added responsiveness to small screens in my recipe page

--- src/app/recipe/my-profile/my-profile.component.ts
+++ src/app/recipe/my-profile/my-profile.component.ts
@@ -11,7 +11,7 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class MyProfileComponent {
   userData: any;
-  defaultprofile="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  defaultprofile="../../../assets/Images/chefimage.jpg";
   constructor(private userService:UserService, private dialog: MatDialog,private router: Router) {}
   edit() {
     const dialogRef = this.dialog.open(EditMyProfileComponent, {
