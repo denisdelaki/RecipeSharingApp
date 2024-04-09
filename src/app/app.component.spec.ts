40655a8dadf1843d808c37946c2429439504beb4
denisdelaki <denisdelaki@gmail.com>
2024-04-09 09:37:30 +0300

    jest test environment set up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have as title 'RecipeSharingApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('RecipeSharingApp');
+    expect(component.title).toEqual('RecipeSharingApp');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, RecipeSharingApp');
+  it('should render title in a h1 tag', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('RecipeSharingApp');
   });
 });
---
+++ src/setup.jest.ts
@@ -0,0 +1,10 @@
+// setup-jest.ts
+import 'jest-preset-angular/setup-jest';
+// src/setup.jest.ts
+
+// import 'jest-extended';
+// src/setup.jest.ts
+
+// module.exports = {
+//     setupFilesAfterEnv: ['<rootDir>/src/setup.jest.ts'],
+//   };
