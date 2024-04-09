f42a84cab83003e53b0d74429ceeeb49de7994a4
denisdelaki <denisdelaki@gmail.com>
2024-04-09 10:43:33 +0300

    created the shared module and added navigation component to it

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { NavigationComponent } from './Components/navigation/navigation.component';
+
+
+@NgModule({
+  declarations: [
+    NavigationComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedRoutingModule
+  ],
+  exports: [
+    NavigationComponent
+  ],
+})
+export class SharedModule { }
