619f499492b58c4d00830d4a10d89685a234d5e1
denisdelaki <denisdelaki@gmail.com>
2024-04-10 14:44:50 +0300

    created my profile page

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { FeaturesRoutingModule } from './features-routing.module';
 import { MyprofileComponent } from './myprofile/myprofile.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -11,6 +16,7 @@ import { MyprofileComponent } from './myprofile/myprofile.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatIconModule,MatButtonModule, MatLabel,
     FeaturesRoutingModule
   ]
 })
