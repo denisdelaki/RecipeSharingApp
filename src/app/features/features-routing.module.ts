6f0e8c9c7e3bb6b3fa2b62432ba7bd8c8f74bef3
denisdelaki <denisdelaki@gmail.com>
2024-04-11 07:44:32 +0300

    created cards to hold recipes contents

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MyprofileComponent } from './Components/myprofile/myprofile.component';
+import { RecipesComponent } from './Components/recipes/recipes.component';
 
 const routes: Routes = [
-  {path: 'myprofile', component: MyprofileComponent}
+  {path: 'myprofile', component: MyprofileComponent},
+  {path: 'allrecipes', component: RecipesComponent}
 ];
 
 @NgModule({
