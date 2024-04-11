a4820a9d8bdd9bae6278a5fe06f56340a3e37b9e
denisdelaki <denisdelaki@gmail.com>
2024-04-11 14:57:00 +0300

    solved the form error in my profile component

--- src/app/features/Components/myprofile/myprofile.component.ts
+++ src/app/features/Components/myprofile/myprofile.component.ts
@@ -2,42 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { EditProfileComponent } from '../edit-profile/edit-profile.component';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UsersService } from '../../Services/users.service';
 
 @Component({
   selector: 'app-myprofile',
   templateUrl: './myprofile.component.html',
-  styleUrl: './myprofile.component.css'
+  styleUrls: ['./myprofile.component.css']
 })
-export class MyprofileComponent implements OnInit{
-changeCred!: FormGroup<any>;
+export class MyprofileComponent implements OnInit {
+  changeCred: FormGroup; 
   userData: any;
-defaultprofile="https://github.com/Eb-Developer-Playground/Recipe_share_app/blob/myrecipes/src/assets/Images/chefimage.jpg?raw=true";
+  defaultprofile = "https://github.com/Eb-Developer-Playground/Recipe_share_app/blob/myrecipes/src/assets/Images/chefimage.jpg?raw=true";
 
-constructor(private router: Router, private dialog: MatDialog,
-  private userservice: UsersService
-){
-  
-}
-  //edit the user profile 
-  Edit(){
+  constructor(
+    private router: Router, 
+    private formBuilder: FormBuilder,
+    private dialog: MatDialog,
+    private userservice: UsersService
+  ) { 
+    this.changeCred = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+ 
+  // Edit the user profile 
+  Edit() {
     const dialogRef = this.dialog.open(EditProfileComponent, {
-      width: '550px', 
-       // Prevent closing by clicking outside or pressing ESC
+      width: '550px',
+      // Prevent closing by clicking outside or pressing ESC
       disableClose: true
     });
-  } 
+  }
 
   ngOnInit(): void {
-    //get logged in userid
+    // Fetch user data based on user ID
     const userId = localStorage.getItem('loggedInUserId');
-    if (userId) {
-      // Fetch user data based on user ID
-      this.userservice.getuserData(userId).subscribe(data => {
-        // Process the fetched user data here
-        console.log(data);
-        this.userData=data;
+    console.log(userId)
+    if (userId !=="" && userId !==undefined) {
+      this.userservice.getuserData(userId || '').subscribe(data => {
+        this.userData = data;
+        console.log(this.userData);
       });
     } else {
       console.error('User ID not found in local storage');
