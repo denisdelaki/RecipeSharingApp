9698082af07efc3a771fc6531029f9e4db305a06
denisdelaki <denisdelaki@gmail.com>
2024-04-12 08:43:42 +0300

    implemented the function to add new recipes via the service

--- src/app/features/Components/new-recipe/new-recipe.component.ts
+++ src/app/features/Components/new-recipe/new-recipe.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
-
+import { RecipesService } from '../../Services/recipes.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-new-recipe',
   templateUrl: './new-recipe.component.html',
@@ -9,19 +11,30 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class NewRecipeComponent {
   AddRecipe!: FormGroup;
+  recipesdata: any[] = [];
 
   constructor(
-    public dialogRef: MatDialogRef<NewRecipeComponent>,private formBuilder: FormBuilder, 
+    public dialogRef: MatDialogRef<NewRecipeComponent>,
+    private formBuilder: FormBuilder, 
+    private recipesService: RecipesService,
+    private snackBar: MatSnackBar,
+    private router: Router
   ){
     this.AddRecipe = this.formBuilder.group({
-      title: [''],
-      ingredients: [''],
-      instructions: [''],
-      recipePicture: [''],
-      category: ['']
+      title: ['', [Validators.required]],
+      ingredients: ['', [Validators.required]],
+      instructions: ['', [Validators.required]],
+      recipePicture: ['', [Validators.required]],
+      category: ['', [Validators.required]],
     });
    }
 
+   openSnackBar(message: string, panelClass: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 1000, 
+      panelClass: [panelClass],
+    });
+  }
   //category optons 
 Categorys = [
   {value: 'BreakFast', viewValue: 'BreakFast'},
@@ -30,7 +43,32 @@ Categorys = [
   {value: 'Snack', viewValue: 'Snack'}
   ];
 add() {
-throw new Error('Method not implemented.');
+console.log(this.AddRecipe.valid);
+if (this.AddRecipe.valid){
+  const recipeData = {
+    title: this.AddRecipe.value.title,
+    ingredients: this.AddRecipe.value?.ingredients,
+    instructions: this.AddRecipe.value?.instructions,
+    recipePicture: this.AddRecipe.value?.recipePicture,
+    category: this.AddRecipe.value?.category
+  }
+  console.log(recipeData);
+  this.recipesService.createRecipes(recipeData).subscribe(
+    response => {
+      console.log(response)
+      this.recipesdata.push({
+          title: response.title,
+          category: response.category,
+          ingredients: response.ingredients,
+          instruction: response.instruction,
+          recipeUrl: response.recipeUrl
+      })
+      this.openSnackBar('Recipe created successfully', 'success-notification');
+      // this.router.navigate(['/recipes/myrecipe'])
+      
+    })
+  this.dialogRef.close();
+}
 }
 close() {
   this.dialogRef.close();
