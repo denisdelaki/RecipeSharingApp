f554e94ed1f83139bbead60606a9b19e433e6489
denisdelaki <denisdelaki@gmail.com>
2024-04-12 13:03:54 +0300

    fetched the recipes data dynamically

--- src/app/features/Components/recipes/recipes.component.ts
+++ src/app/features/Components/recipes/recipes.component.ts
@@ -1,18 +1,54 @@
 import { Component } from '@angular/core';
 import { NewRecipeComponent } from '../new-recipe/new-recipe.component';
 import { MatDialog } from '@angular/material/dialog';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject, takeUntil, delay } from 'rxjs';
+import { RecipesService } from '../../Services/recipes.service';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
 export class RecipesComponent {
-
+  isMyRecipes!: boolean;
+  recipesData: any;
   constructor(
     private dialog: MatDialog,
-
+    private route: ActivatedRoute,
+    private router: Router,
+    private recipesService: RecipesService
   ) { 
+    this.route.url.subscribe(url => {
+      this.isMyRecipes = (url[0].path === 'myrecipes');
+      });
+  }
+
+  private destroy$: Subject<void> = new Subject<void>();
+  ngOnInit(): void {
+   this.loadrecipes() 
+
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  //load the recipes
+  loadrecipes() {
+    const userId = localStorage.getItem('loggedInUserId') ?? '';
+    //load the current logged in user's recipes
+    if (this.isMyRecipes) {
+      this.recipesService.getmyRecipe(userId).pipe(takeUntil(this.destroy$)).subscribe((data: any) => {
+        this.recipesData = data;
+        console.log(this.recipesData);
+      })
+    }else{
+      //Show all the recipes 
+      this.recipesService.getrecipes().pipe(takeUntil(this.destroy$)).subscribe((data: any) => {
+        this.recipesData = data;
+        console.log(this.recipesData);
+      })
+    }
 
   }
   newrecipe(){
