2595304b34a9d17bd972cd7b560b5b00d04cb61f
denisdelaki <denisdelaki@gmail.com>
2024-04-16 10:57:09 +0300

    added routes to the landing page

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+import path from 'node:path';
+import { TestimonialsComponent } from './Components/testimonials/testimonials.component';
 
 const routes: Routes = [
-  {path: '', component:LandingPageComponent}
+  {path: '', component:LandingPageComponent},
+  {path: 'testimonials', component:TestimonialsComponent}
 ];
 
 @NgModule({
