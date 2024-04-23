ee9844a7e686f02f18c578b4a80ad40abea01d94
denisdelaki <denisdelaki@gmail.com>
2024-04-23 16:40:29 +0300

    added suite tests

--- src/app/features/Components/editrecipe/editrecipe.component.spec.ts
+++ src/app/features/Components/editrecipe/editrecipe.component.spec.ts
@@ -1,17 +1,40 @@
-import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
+import { TestBed, ComponentFixture, waitForAsync, fakeAsync, tick } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { EditrecipeComponent } from './editrecipe.component';
 import { RecipesService } from '../../Services/recipes.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'; // Import NoopAnimationsModule
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('EditrecipeComponent', () => {
   let component: EditrecipeComponent;
   let fixture: ComponentFixture<EditrecipeComponent>;
+  let mockMatDialogRef: Partial<MatDialogRef<EditrecipeComponent>>;
+  let mockMatSnackBar: Partial<MatSnackBar>;
+  let mockRecipesService: Partial<RecipesService>;
+  let mockRouter: Partial<Router>;
 
-  beforeEach(
-    waitForAsync(() => {
+  beforeEach(waitForAsync(() => {
+    mockMatDialogRef = {
+      close: jest.fn()
+    };
+
+    mockMatSnackBar = {
+      open: jest.fn()
+    };
+
+    mockRecipesService = {
+      getRecipes: jest.fn(() => of({})),
+      editRecipe: jest.fn(() => of({}))
+    };
+
+    mockRouter = {
+      navigate: jest.fn()
+    };
+
       TestBed.configureTestingModule({
         declarations: [EditrecipeComponent],
         imports: [
@@ -20,9 +43,11 @@ describe('EditrecipeComponent', () => {
           NoopAnimationsModule 
         ],
         providers: [
-          RecipesService,
-          { provide: MatDialogRef, useValue: {} },
-          { provide: MAT_DIALOG_DATA, useValue: {} }
+          { provide: MatDialogRef, useValue: mockMatDialogRef },
+          { provide: MAT_DIALOG_DATA, useValue: {} },
+          { provide: MatSnackBar, useValue: mockMatSnackBar },
+          { provide: RecipesService, useValue: mockRecipesService },
+          { provide: Router, useValue: mockRouter }
         ]
       }).compileComponents();
       fixture = TestBed.createComponent(EditrecipeComponent);
@@ -34,5 +59,27 @@ describe('EditrecipeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should call close() on MatDialogRef when close() is called', () => {
+    // Act
+    component.close();
 
+    // Assert
+    expect(mockMatDialogRef.close).toHaveBeenCalled();
+  });
+  it('should contain categories', () => {
+    // Assert
+    expect(component.Categorys).toBeTruthy();
+  })
+
+  it('should call openSnackBar() on MatSnackBar when openSnackBar() is called', () => {
+    // Act
+    component.openSnackBar('Test message', 'panel-class');
+
+    // Assert
+    expect(mockMatSnackBar.open).toHaveBeenCalledWith('Test message', 'Close', { duration: 1000, panelClass: ['panel-class'] });
+    expect(mockMatSnackBar.open).toHaveBeenCalledTimes(2);
+  })
+  
+ 
+  
 });
--- src/app/features/Components/myprofile/myprofile.component.spec.ts
+++ src/app/features/Components/myprofile/myprofile.component.spec.ts
@@ -4,12 +4,17 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { of } from 'rxjs';
 import { EditProfileComponent } from '../edit-profile/edit-profile.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from '../../Services/users.service';
+import { Router } from '@angular/router';
 
 describe('MyprofileComponent', () => {
   let component: MyprofileComponent;
   let fixture: ComponentFixture<MyprofileComponent>;
   let mockMatDialog: jest.Mocked<MatDialog>;
   let mockDialogRef: any;
+  let router: Router;
+  let userservice: UsersService;
 
   beforeEach(waitForAsync(() => {
     mockMatDialog = {
@@ -18,16 +23,21 @@ describe('MyprofileComponent', () => {
 
     TestBed.configureTestingModule({
       declarations: [MyprofileComponent],
-      imports: [MatDialogModule, HttpClientModule],
+      imports: [MatDialogModule, HttpClientModule, ReactiveFormsModule],
       providers: [
-        { provide: MatDialog, useValue: mockMatDialog }
+        { provide: MatDialog, useValue: mockMatDialog },
+        FormBuilder,
+        UsersService
       ]
     }).compileComponents();
-
+    router=TestBed.inject(Router);
+    userservice=TestBed.inject(UsersService);
     fixture = TestBed.createComponent(MyprofileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
+
+  
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -47,4 +57,40 @@ describe('MyprofileComponent', () => {
       disableClose: true
     });
   });
+   
+  it('should call router.navigate when viewSavedRecipes is called', () => {
+    // Arrange
+   jest.spyOn(router, 'navigate')
+
+    // Act
+    component.viewSavedRecipes();
+
+    // Assert
+    expect(router.navigate).toHaveBeenCalledWith(['/features/favorites']);
+  })
+
+  it('should call userservice.getuserData when ngOnInit is called', () => {
+    // Arrange
+    jest.spyOn(userservice, 'getuserData');
+
+    // Act
+    component.ngOnInit();
+
+    // Assert
+    expect(userservice.getuserData).toHaveBeenCalled();
+  })
+
+  it('should fetch user data from local storage if available', () => {
+    // Arrange
+    localStorage.setItem('loggedInUserId', '123');
+    jest.spyOn(userservice, 'getuserData');
+
+    // Act
+    component.ngOnInit();
+
+    // Assert
+    expect(userservice.getuserData).toHaveBeenCalledWith('123');
+  })
+
+
 });
