c8251ef644ccfad286736543119fdb6e8f763909
denis.kipyegon@equitybank.co.ke <denis.kipyegon@equitybank.co.ke>
2024-05-13 10:23:45 +0300

    hosted the app in the firebase

--- src/app/Environment/environment.ts
+++ src/app/Environment/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     projectId: "recipesharingapp-01",
     storageBucket: "recipesharingapp-01.appspot.com",
     messagingSenderId: "518171464728",
-    appId: "1:518171464728:web:d24c48061db7a667e01f6d",
-    measurementId: "G-YKS1FFL5YC"
+    appId: "1:518171464728:web:3061a517163cc399e01f6d",
+    measurementId: "G-PLQWLD1HJ5"
   }
 };
